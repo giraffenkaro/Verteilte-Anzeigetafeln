655aab715a6dbd012d1ecbe6756095bc7f97fe30
Laura <giraffenkaro@web.de>
2017-08-16 16:50:04 +0200

    Dominiks Code wieder eingefügt

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,40 +1,10 @@
-/*import database.Database;
-import database.exceptions.DatabaseConnectionException;
-import threads.ServerThread;
-
-/**
- * Created by Dominik on 08.07.2017.
- */
-/*
-public class Server {
-
-    public static void main (String[] args){
-        Database database = null;
-        try {
-            database = new Database();
-            /*try {
-                ServerThread server = new ServerThread(database);
-                server.run();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                database.closeDB();
-            }*/
-/*
-        } catch (DatabaseConnectionException e) {
-            e.printStackTrace();
-        }
-    }
-}
-*/
-
-
-
-
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import database.Database;
+import database.exceptions.DatabaseConnectionException;
+import threads.ServerThread;
 
 
 public class Server implements Functions
@@ -44,6 +14,23 @@ public class Server implements Functions
 
     public static void main(String args[])
     {
+
+        Database database = null;
+        try {
+            database = new Database();
+            try {
+                ServerThread server = new ServerThread(database);
+                server.run();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                database.closeDB();
+            }
+
+        } catch (DatabaseConnectionException e) {
+            e.printStackTrace();
+        }
+
 
         try
         {
